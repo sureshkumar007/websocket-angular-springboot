78a2817b9f48eb222bc42923801e3c46093933e4
sureshkumar007 <sureshkumar12074@gmail.com>
2019-08-21 10:38:03 +0530

    subscription and socket is working

--- src/main/java/com/stackroute/socket/rest/SocketRest.java
+++ src/main/java/com/stackroute/socket/rest/SocketRest.java
@@ -21,37 +21,51 @@ public class SocketRest {
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> sendMessage(@RequestBody  Map<String, String> message){
-        if(message.containsKey("message")){
-            if(message.containsKey("toId") && message.get("toId")!=null && !message.get("toId").equals("")){
+    public ResponseEntity<?> sendMessage(@RequestBody  Map<String, String> message)
+    {
+        if(message.containsKey("message"))
+        {
+            //Send to the particular Id(means socket Id)
+            if(message.containsKey("toId") && message.get("toId")!=null && !message.get("toId").equals(""))
+            {
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+message.get("toId"),message);
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+message.get("fromId"),message);
-            }else{
+            }
+            //If id is not mentioned then the message should received by all members
+            else
+            {
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher",message);
             }
+            //Creating class for that entity
             return new ResponseEntity<>(message, new HttpHeaders(), HttpStatus.OK);
         }
         return new ResponseEntity<>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
-
+//Mapping the message
     @MessageMapping("/send/message")
     public Map<String, String> broadcastNotification(String message){
         ObjectMapper mapper = new ObjectMapper();
         Map<String, String> messageConverted = null;
-        try {
+        try
+        {
             messageConverted = mapper.readValue(message, Map.class);
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             messageConverted = null;
         }
-        if(messageConverted!=null){
-            if(messageConverted.containsKey("toId") && messageConverted.get("toId")!=null && !messageConverted.get("toId").equals("")){
+        if(messageConverted!=null)
+        {
+            if(messageConverted.containsKey("toId") && messageConverted.get("toId")!=null && !messageConverted.get("toId").equals(""))
+            {
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+messageConverted.get("toId"),messageConverted);
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher/"+messageConverted.get("fromId"),message);
-            }else{
+            }
+            else
+            {
                 this.simpMessagingTemplate.convertAndSend("/socket-publisher",messageConverted);
             }
         }
         return messageConverted;
     }
-
 }
